f5dc3dd605a995528475631dd0d4e4cfb4573cf7
Bhargava Srinarasi <bhargavabhat@gmail.com>
2016-05-01 21:59:56 +0200

    correcting the domain name

--- daily_mailer.py
+++ daily_mailer.py
@@ -5,7 +5,7 @@ def send_simple_message():
     print "Journal Mail <journalmail@{}>".format(os.environ['MAILGUN_DOMAIN'])
 
     return requests.post(
-        "https://api.mailgun.net/v3/YOUR_DOMAIN_NAME/messages",
+        "https://api.mailgun.net/v3/{}/messages".format(os.environ['MAILGUN_DOMAIN']),
         auth=("api", os.environ['MAILGUN_API_KEY']),
         data={"from": "Journal Mail <journalmail@{}>".format(os.environ['MAILGUN_DOMAIN']),
               "to": [os.environ['MAIL_RECIPIENT']],
